7486ccddf0f732f010878e7b9106c19805ed002a
petersenrj <petersenrr@Ryans-MacBook-Pro.local>
2016-04-03 10:42:43 -0600

    Added my files to the project

--- clook.java
+++ clook.java
@@ -1,8 +1,105 @@
+/* Promise of Originality
+ I promise that this source code file has, in it's entirety, been
+ written by myself or a member of my group and by no other person or
+ persons. If at any time an exact copy of this source code is found to
+ be used by another person outside my group in this term, I understand
+ that all members of my group and the members of the group that
+ submitted the copy will receive a zero on this assignment.
+ */
+
+
+package clook;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ *
+ * @author petersenrr
+ */
 public class clook {
-
-     public static void main(String[] args){
-         int total = 0;
-         System.out.println("C-LOOK Total Seek: " + total);
-      }
-
- }
+    
+    private static ArrayList<Integer> array;
+    private static int total = 0;
+    
+    
+    public static void main(String[] args) throws FileNotFoundException, IOException
+    {
+        
+        File file = null;
+        if (0 < args.length)
+        {
+            file = new File(args[0]);
+        }
+        
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bufferReader = new BufferedReader(fileReader);
+        clook.array = new ArrayList<>();
+        String line;
+        
+        while((line = bufferReader.readLine()) !=null)
+        {
+            
+            int temp = Integer.parseInt(line);
+            clook.array.add(temp);
+        }
+        calculate();
+        
+        System.out.println("SSTF Total Seek: " + clook.total);
+    }
+    
+    private static void calculate()
+    {
+        int temp = clook.array.get(0);
+        Collections.sort(clook.array);
+        int i = 0;
+        int next = 0;
+        int current = 0;
+        
+        while(clook.array.get(i) != null)
+        {
+            if(temp == clook.array.get(i))
+            {
+                next = i+1;
+                current = i;
+                break;
+            }
+            i++;
+        }
+        
+        for(int j = 0; j < clook.array.size()-1; j++)
+        {
+            if(clook.array.get(next) > clook.array.get(current))
+            {
+                clook.total += (clook.array.get(next) - clook.array.get(current));
+                current = next;
+                if(next == (clook.array.size()-1))
+                {
+                    next = 0;
+                }
+                else
+                {
+                    next = next + 1;
+                }
+            }
+            else
+            {
+                clook.total += (clook.array.get(current) - clook.array.get(next));
+                current = next;
+                if(next == (clook.array.size()-1))
+                {
+                    next = 0;
+                }
+                else
+                {
+                    next = next + 1;
+                }
+            }
+        }
+    }
+}
--- ssft.java
+++ ssft.java
@@ -1,8 +1,113 @@
-public class ssft {
+/* Promise of Originality
+ I promise that this source code file has, in it's entirety, been
+ written by myself or a member of my group and by no other person or
+ persons. If at any time an exact copy of this source code is found to
+ be used by another person outside my group in this term, I understand
+ that all members of my group and the members of the group that
+ submitted the copy will receive a zero on this assignment.
+ */
 
-     public static void main(String[] args){
-         int total = 0;
-         System.out.println("SSTF Total Seek: " + total);
-      }
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
- }
+public class ssft
+{
+    
+    private static ArrayList<Integer> array;
+    private static int total = 0;
+    
+    
+    public static void main(String[] args) throws FileNotFoundException, IOException{
+        File file = null;
+        if (0 < args.length)
+        {
+            file = new File(args[0]);
+        }
+        
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bufferReader = new BufferedReader(fileReader);
+        array = new ArrayList<>();
+        String line;
+        
+        while((line = bufferReader.readLine()) !=null)
+        {
+            
+            int temp = Integer.parseInt(line);
+            array.add(temp);
+        }
+        
+        int current = 0;
+        
+        while(ssft.array.size() >= 2)
+        {
+            current = compare(current);
+        }
+        System.out.println("SSTF Total Seek: " + total);
+    }
+    
+    private static int compare(int current)
+    {
+        
+        int currentIndex = current;
+        
+        int currentValue;
+        int next = currentIndex + 1;
+        if(currentIndex == array.size()-1)
+        {
+            next = 0;
+        }
+        
+        int count = 0;
+        int newIndex = 1;
+        int lowestTemp = -1;
+        if(next < array.size())
+        {
+            lowestTemp = Math.abs(ssft.array.get(currentIndex) - ssft.array.get(next));
+            newIndex = next;
+        }
+        while(count < array.size()-1)
+        {
+            if(next < array.size())
+            {
+                if(Math.abs(ssft.array.get(currentIndex) - ssft.array.get(next)) <= lowestTemp)
+                {
+                    lowestTemp = Math.abs(ssft.array.get(currentIndex) - ssft.array.get(next));
+                    newIndex = next;
+                    
+                }
+                
+            }
+            if(next == ssft.array.size()-1)
+            {
+                next = 0;
+            }
+            else
+            {
+                next = next + 1;
+            }
+            count++;
+        }
+        
+        total += lowestTemp;
+        System.out.println("newIndex = " + ssft.array.get(newIndex) + " removed = " + ssft.array.get(currentIndex) +
+                           " sum = " + total);
+        currentValue = ssft.array.get(newIndex);
+        array.remove(currentIndex);
+        
+        for(int i = 0; i < array.size(); i++)
+        {
+            if(ssft.array.get(i) == currentValue)
+            {
+                currentIndex = i;
+                break;
+            }
+        }
+        return currentIndex;
+    }
+    
+    
+}
